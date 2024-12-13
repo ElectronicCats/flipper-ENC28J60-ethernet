96cbb9585ce91156ec390d18f2020d93b9dbc5e2
Donuts Diaz <carlosadonai.de0208@gmail.com>
2024-12-13 10:13:16 -0600

    test: run program

--- EthernetAppDemo/app_user.c
+++ EthernetAppDemo/app_user.c
@@ -1,7 +1,13 @@
 #include "app_user.h"
 
+uint8_t MAC[] = {255, 255, 255, 255, 255, 255};
+
 int app_main(void* p) {
     UNUSED(p);
+
+    enc28j60_t* ethernet = enc28j60_alloc(MAC);
+
+    free_enc28j60(ethernet);
 
     return 0;
 }
