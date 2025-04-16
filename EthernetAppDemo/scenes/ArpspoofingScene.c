87f7b94a2c6bcd461b9a8b14c0d64bf0edcb1758
AdonaiDiaz <carlosadonai.de0208@gmail.com>
2025-04-16 12:32:21 -0600

    feat: add thread function for the ArpSpoofing

--- EthernetAppDemo/scenes/ArpspoofingScene.c
+++ EthernetAppDemo/scenes/ArpspoofingScene.c
@@ -1,8 +1,18 @@
 #include "../app_user.h"
 
+/**
+ * In this file you will find the ARP Spoofing scene and it worker,
+ * for the usage of I played with the view dispatcher and the thread to show different
+ * views for not use many scenes and avoid to define many 
+ */
+
+// ArpSpoofing thread
+int32_t arpspoofing_thread(void* context);
+
 // ArpSpoofing on enter
 void app_scene_arp_spoofing_on_enter(void* context) {
-    UNUSED(context);
+    App* app = (App*)context;
+    app->thread = furi_thread_alloc_ex("ArpSpoofing", 2 * 1024, arpspoofing_thread, app);
 }
 
 // ArpSpoofing on event
@@ -14,7 +24,27 @@ bool app_scene_arp_spoofing_on_event(void* context, SceneManagerEvent event) {
     return consumed;
 }
 
+// ArpSpoofing on exit
 void app_scene_arp_spoofing_on_exit(void* context) {
     App* app = (App*)context;
-    UNUSED(app);
+    furi_thread_join(app->thread);
+    furi_thread_free(app->thread);
+}
+
+/**
+ * Main Thread for the ArpSpoofing
+ * Here is where the main code comes for the worker
+ */
+int32_t arpspoofing_thread(void* context) {
+    App* app = (App*)context;
+
+    enc28j60_t* ethernet = app->ethernet;
+
+    widget_reset(app->widget);
+
+    bool start = enc28j60_start(ethernet) != 0xff;
+
+    UNUSED(start);
+
+    return 0;
 }
