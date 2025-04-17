2d8f073c119f6097aeaf3bbb1279438666b8d2f7
AdonaiDiaz <carlosadonai.de0208@gmail.com>
2025-04-17 16:08:38 -0600

    feat: add view when th network is not connected

--- EthernetAppDemo/draw_functions/general_draws.c
+++ EthernetAppDemo/draw_functions/general_draws.c
@@ -19,7 +19,7 @@ void draw_device_no_connected(App* app) {
 // Draws if the network is not link
 void draw_network_not_connected(App* app) {
     widget_reset(app->widget);
-    // widget_add_icon_element(app->widget, 4, 0, &I_NOC119x38); Needs to change the icon
+    widget_add_icon_element(app->widget, 41, 0, &I_NCR);
     widget_add_string_multiline_element(
-        app->widget, 64, 32, AlignCenter, AlignCenter, FontPrimary, "Network\nNot Detected");
+        app->widget, 64, 60, AlignCenter, AlignBottom, FontPrimary, "Network\nNot Detected");
 }
