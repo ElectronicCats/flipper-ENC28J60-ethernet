5630314a24e0bdddc6d58475f8a8bb9271c84f29
AdonaiDiaz <carlosadonai.de0208@gmail.com>
2025-04-17 13:41:18 -0600

    feat: add icons

--- EthernetAppDemo/draw_functions/general_draws.c
+++ EthernetAppDemo/draw_functions/general_draws.c
@@ -11,9 +11,9 @@ void draw_in_development(App* app) {
 // Draws device not connected
 void draw_device_no_connected(App* app) {
     widget_reset(app->widget);
-    // widget_add_icon_element(app->widget, 4, 0, &I_NOC119x38); Needs to change the icon
+    widget_add_icon_element(app->widget, 4, 0, &I_NOC119x38);
     widget_add_string_multiline_element(
-        app->widget, 64, 32, AlignCenter, AlignCenter, FontPrimary, "DEVICE NOT\nCONNECTED");
+        app->widget, 65, 60, AlignCenter, AlignBottom, FontPrimary, "DEVICE NOT\nCONNECTED");
 }
 
 // Draws if the network is not link
